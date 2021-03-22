a714da4fbc93fa7f5c73f6a7047b8b1140872a9b
rocketpy <dimitr.ushakov@gmail.com>
2021-03-22 22:53:22 +0200

    Update eli5_visualize_and_debug.py

--- eli5_visualize_and_debug.py
+++ eli5_visualize_and_debug.py
@@ -1,1 +1,21 @@
+#  Debug machine learning classifiers and explain their predictions
+
+# PyPi: https://pypi.org/project/eli5/
+# Docs: https://eli5.readthedocs.io/en/latest/
+
+# pip install eli5
+
+"""
+Basic Usage
+There are two main ways to look at a classification or a regression model:
+
+1. inspect model parameters and try to figure out how the model works globally;
+2. inspect an individual prediction of a model, try to figure out why the model makes the decision it makes.
+
+For (1) ELI5 provides eli5.show_weights() function; for (2) it provides eli5.show_prediction() function.
+"""
+
+import eli5
+
+eli5.show_weights(clf)
 
